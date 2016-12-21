b56a5db158056c8f470ca5568e8be0dc4aee15db
PrimalNox <xbox_live81218@hotmail.com>
2016-12-21 13:08:06 -0700

    files

---
+++ src/Prob1.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rndm = new Random();
+		for(int j = 0; j < 33; j++){
+			int r = 71 + rndm.nextInt(29);
+			System.out.println(r);
+		}
+	}
+
+}
